ad4c43eb72783829ca0fe6383366b78dcc3e4896
Fran Panteli <francescapanteli@gmail.com>
2024-04-03 22:51:44 +0100

    This commit pushes changes to the project .py file.

--- verify_card_number.py
+++ verify_card_number.py
@@ -17,18 +17,18 @@
 def verify_card_number(card_number):
 
 """
-Initialising variables:
-	-> We aren't running any module imports when defining this function 
-	-> The first stage of defining this function is to initialise variables <- Three of them 
-	-> The first is for the sum of the odd digits of the card number (the function input), initialised at zero 
-	-> The credit card number is the input to the function 
-	-> The second variable we are initialising is this number backwards (all of the elements, stored in descending 
-        order)
-	-> Then the third variable is the `odd_digits`
-		-> This is the card number going backwards,  but the even integers of this 
-		-> This also includes all of the numbers on the card (we aren't slicing out some of these numbers)
-	-> So we can now store the sum of the odd digits and the sum of the even ones <- We have variables initialised 
-        to do this with the credit card number, which is the input to the function 
+    Initialising variables:
+        -> We aren't running any module imports when defining this function 
+        -> The first stage of defining this function is to initialise variables <- Three of them 
+        -> The first is for the sum of the odd digits of the card number (the function input), initialised at zero 
+        -> The credit card number is the input to the function 
+        -> The second variable we are initialising is this number backwards (all of the elements, stored in descending 
+            order)
+        -> Then the third variable is the `odd_digits`
+            -> This is the card number going backwards,  but the even integers of this 
+            -> This also includes all of the numbers on the card (we aren't slicing out some of these numbers)
+        -> So we can now store the sum of the odd digits and the sum of the even ones <- We have variables initialised 
+            to do this with the credit card number, which is the input to the function 
 """
 
     sum_of_odd_digits = 0
@@ -36,67 +36,67 @@ Initialising variables:
     odd_digits = card_number_reversed[::2]
 
 """
-Building the sum_of_odd_digits variable: 
--> `odd_digits` is the variable which contains the odd digits in the credit card number <- The input to the function 
--> We are iterating through each of the digits in this larger number, to calculate the value of the sum this variable 
-    stores 
--> We are adding them with each iteration to a counter, called `sum_of_odd_digits`
--> It's a way of taking a longer number which is composed of many digits, iterating through each of those digits and 
-    with each iteration adding the number to a counter 
--> So after this, we now have the sum of the odd digits in the credit card, which we store in the `sum_of_odd_digits` 
-    variable 
+    Building the sum_of_odd_digits variable: 
+        -> `odd_digits` is the variable which contains the odd digits in the credit card number <- The input to the function 
+        -> We are iterating through each of the digits in this larger number, to calculate the value of the sum this variable 
+            stores 
+        -> We are adding them with each iteration to a counter, called `sum_of_odd_digits`
+        -> It's a way of taking a longer number which is composed of many digits, iterating through each of those digits and 
+            with each iteration adding the number to a counter 
+        -> So after this, we now have the sum of the odd digits in the credit card, which we store in the `sum_of_odd_digits` 
+            variable 
 """
 
     for digit in odd_digits:
         sum_of_odd_digits += int(digit)
 
 """
-Initialising the value of variables (continued): 
--> We have one variable for the sum of the even digits 
--> And then we have another variable, which stores the even digits in the card number (function argument)
--> 'Even digit' means the index that the number is stored at in the function input is even, not that the number which 
-    is stored at the index is even 
--> We are now repeating the process we just did for the odd numbers, but for the even numbers
--> Initialising the sum of these numbers as 0, in the `sum_of_even_digits` variable 
--> And then even_digits <- This variable stores those numbers themselves
--> Start from the second number (zero-indexed) in the input string, and then increase in increments of 2 
--> Do this for the entire input string, and now we have a list of all of the numbers in the input string which were 
-    stored at even indices in it 
--> This is an example of slicing 
+    Initialising the value of variables (continued): 
+        -> We have one variable for the sum of the even digits 
+        -> And then we have another variable, which stores the even digits in the card number (function argument)
+        -> 'Even digit' means the index that the number is stored at in the function input is even, not that the number which 
+            is stored at the index is even 
+        -> We are now repeating the process we just did for the odd numbers, but for the even numbers
+        -> Initialising the sum of these numbers as 0, in the `sum_of_even_digits` variable 
+        -> And then even_digits <- This variable stores those numbers themselves
+        -> Start from the second number (zero-indexed) in the input string, and then increase in increments of 2 
+        -> Do this for the entire input string, and now we have a list of all of the numbers in the input string which were 
+            stored at even indices in it 
+        -> This is an example of slicing 
 """
 
     sum_of_even_digits = 0
     even_digits = card_number_reversed[1::2]
 
 """
-In the context of the wider .py file: 
-	-> We are trying to validate that the input to the function, which is a credit card number, is a real credit card 
-    number
-	-> We are doing this using the Luhn algorithm
-	-> We need to calculate the sum of the integers which are stored at even indices in the input number and doing the 
-        same with the numbers stored at the odd integers
-	-> We then add these two together and calculate their sum
-	-> We perform a modulo 10 operation on this in relation to 10 <- In which case, depending on the output, the card 
-        number is valid or invalid 
-    -> The first function which we are defining in this .py file is for the card number validator and the second function 
-    is for testing this 
+    In the context of the wider .py file: 
+        -> We are trying to validate that the input to the function, which is a credit card number, is a real credit card 
+        number
+        -> We are doing this using the Luhn algorithm
+        -> We need to calculate the sum of the integers which are stored at even indices in the input number and doing the 
+            same with the numbers stored at the odd integers
+        -> We then add these two together and calculate their sum
+        -> We perform a modulo 10 operation on this in relation to 10 <- In which case, depending on the output, the card 
+            number is valid or invalid 
+        -> The first function which we are defining in this .py file is for the card number validator and the second function 
+        is for testing this 
 
-This next block of code in the first function this .py file defines:
-	-> The argument of the function is a credit card number 
-	-> We are iterating through each of the digits stored at even indices of this number 
-	-> Most of the code inside this block then codes the mathematics of the Luhn algorithm into Python 
-	-> We are iterating through all of the digits which are stored at even indices in the credit card
-	-> We times each of these numbers by 2 
-	-> Then we perform modulo operations to apply the Luhn algorithm
-	-> More information about the precise steps of this is in the project problem-solving thought process notes file <- There 
-        are broadly three steps to this in this case
-	-> Then we add this number to the `sum_of_even_digits` variable 
-	-> We are updating the value of this variable with each digit we iterate through 
+    This next block of code in the first function this .py file defines:
+        -> The argument of the function is a credit card number 
+        -> We are iterating through each of the digits stored at even indices of this number 
+        -> Most of the code inside this block then codes the mathematics of the Luhn algorithm into Python 
+        -> We are iterating through all of the digits which are stored at even indices in the credit card
+        -> We times each of these numbers by 2 
+        -> Then we perform modulo operations to apply the Luhn algorithm
+        -> More information about the precise steps of this is in the project problem-solving thought process notes file <- There 
+            are broadly three steps to this in this case
+        -> Then we add this number to the `sum_of_even_digits` variable 
+        -> We are updating the value of this variable with each digit we iterate through 
 
-Updating sums:
-	-> After the value of this variable has been updated, we then update the value of the `total` variable 
-	-> This is the sum of the even digits and the sum of the odd digits 
-	-> Then return this put into a boolean expression with the modulo 10 operator, to apply the Luhn algorithm
+    Updating sums:
+        -> After the value of this variable has been updated, we then update the value of the `total` variable 
+        -> This is the sum of the even digits and the sum of the odd digits 
+        -> Then return this put into a boolean expression with the modulo 10 operator, to apply the Luhn algorithm
 """
 
     for digit in even_digits:
